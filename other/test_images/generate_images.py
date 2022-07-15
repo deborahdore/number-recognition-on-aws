0702df42a25a98580f0c8918ce31db074d5a6a92
VincenzoImp <vincenzoimperati99@gmail.com>
2022-07-15 14:45:52 +0200

    update test images

---
+++ other/test_images/generate_images.py
@@ -0,0 +1,9 @@
+from PIL import Image
+size = 28
+increase = 70
+n_images = 100
+img = Image.open('images/img28.png')
+for i in range(n_images):
+    size = size + increase
+    resized_img = img.resize((size, size))
+    resized_img.save('images/img{}.png'.format(size))
